68a036bfa250db30c1ad689b39a64a1a8d737517
faizakhirad840 <pc14779.ammar@gmail.com>
2024-05-15 22:37:50 -0700

    Adding more options

--- index.ts
+++ index.ts
@@ -9,21 +9,25 @@ const answer = await inquirer.prompt([
     message: "Choose one of the operator to perform operation",
     type: "list",
     name: "operator",
-    choices : ["addition", "subtraction", "multiplication", "division"],
+    choices : ["addition", "subtraction", "multiplication", "division", "modulus", "exponentiation"],
   },
 ]);
 
 if(answer.operator === "addition"){
-    console.log(`The output is ${answer.FirstNumber + answer.secondNumber}`);
+    console.log(`The result is ${answer.FirstNumber + answer.secondNumber}`);
     
 }
 else if(answer.operator === "subtraction"){
-    console.log(`The output is ${answer.FirstNumber - answer.secondNumber}`);
+    console.log(`The result is ${answer.FirstNumber - answer.secondNumber}`);
 }
 else if(answer.operator === "multiplication"){
-    console.log(`The output is ${answer.FirstNumber * answer.secondNumber}`);
+    console.log(`The result is ${answer.FirstNumber * answer.secondNumber}`);
 }
 else if(answer.operator === "division"){
-    console.log(`The output is ${answer.FirstNumber / answer.secondNumber}`);
+    console.log(`The result is ${answer.FirstNumber / answer.secondNumber}`);
 }
-
+else if(answer.operator === "modulus"){
+  console.log(`The result is ${answer.FirstNumber % answer.secondNumber}`);
+}else if(answer.operator === "exponentiation"){
+  console.log(`The result is ${answer.FirstNumber ** answer.secondNumber}`);
+}
